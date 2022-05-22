a0d0814fc006e8b499e84b9ac5468b69e69a15db
Hải Ngọc Bùi <ngoc200h@gmail.com>
2022-05-23 02:14:38 +0700

    sua

--- BTL_Nhom4_De3/frmDSMon.Designer.cs
+++ BTL_Nhom4_De3/frmDSMon.Designer.cs
@@ -30,6 +30,16 @@ namespace BTL_Nhom4_De3
         private void InitializeComponent()
         {
             this.dgvMon = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTenMon = new System.Windows.Forms.TextBox();
+            this.txtMaMon = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.txtDVHT = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMon)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,29 +50,139 @@ namespace BTL_Nhom4_De3
             this.dgvMon.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvMon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvMon.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.dgvMon.Location = new System.Drawing.Point(0, 136);
+            this.dgvMon.Location = new System.Drawing.Point(0, 166);
             this.dgvMon.MultiSelect = false;
             this.dgvMon.Name = "dgvMon";
             this.dgvMon.ReadOnly = true;
             this.dgvMon.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvMon.Size = new System.Drawing.Size(579, 202);
+            this.dgvMon.Size = new System.Drawing.Size(579, 172);
             this.dgvMon.TabIndex = 22;
+            this.dgvMon.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMon_CellClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(321, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 53;
+            this.label3.Text = "ĐVHT";
+            // 
+            // txtTenMon
+            // 
+            this.txtTenMon.Location = new System.Drawing.Point(185, 72);
+            this.txtTenMon.Name = "txtTenMon";
+            this.txtTenMon.Size = new System.Drawing.Size(100, 20);
+            this.txtTenMon.TabIndex = 52;
+            // 
+            // txtMaMon
+            // 
+            this.txtMaMon.Location = new System.Drawing.Point(185, 37);
+            this.txtMaMon.Name = "txtMaMon";
+            this.txtMaMon.Size = new System.Drawing.Size(100, 20);
+            this.txtMaMon.TabIndex = 51;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(113, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 50;
+            this.label2.Text = "Tên Môn";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 49;
+            this.label1.Text = "Mã Môn";
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Location = new System.Drawing.Point(435, 120);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(75, 23);
+            this.btnLuu.TabIndex = 48;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(317, 120);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 47;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(185, 120);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 46;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(66, 120);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 45;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // txtDVHT
+            // 
+            this.txtDVHT.Location = new System.Drawing.Point(377, 37);
+            this.txtDVHT.Name = "txtDVHT";
+            this.txtDVHT.Size = new System.Drawing.Size(100, 20);
+            this.txtDVHT.TabIndex = 54;
             // 
             // frmDSMon
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(579, 338);
+            this.Controls.Add(this.txtDVHT);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTenMon);
+            this.Controls.Add(this.txtMaMon);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThem);
             this.Controls.Add(this.dgvMon);
             this.Name = "frmDSMon";
             this.Text = "Danh Sách Môn";
             this.Load += new System.EventHandler(this.frmDSMon_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvMon)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.DataGridView dgvMon;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTenMon;
+        private System.Windows.Forms.TextBox txtMaMon;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.TextBox txtDVHT;
     }
 }
--- BTL_Nhom4_De3/frmDSMon.cs
+++ BTL_Nhom4_De3/frmDSMon.cs
@@ -20,22 +20,87 @@ namespace BTL_Nhom4_De3
         {
             LoadDSMon();
         }
-        private string tukhoa = "";
+        Database dt = new Database();
+        private void Reset()
+        {
+            txtMaMon.Text = "";
+            txtTenMon.Text = "";
+            txtDVHT.Text = "";
+        }
         private void LoadDSMon()
         {
-            List<CustomParameter> lstPara = new List<CustomParameter>();
-            lstPara.Add(new CustomParameter()
-            {
-                key = "@tukhoa",
-                value = tukhoa
-            }
-                                            );
-            //dgvSinhVien.DataSource = new Database().SelectData("SelectAllKhoa", lstPara);
-            dgvMon.DataSource = new Database().Mon();
+            string sql1 = "select * from Mon";
+            dgvMon.DataSource = dt.TaoBang(sql1);
             //Mon (MaMon, TenMon, DVHT)
             dgvMon.Columns["MaMon"].HeaderText = "Mã Môn";
             dgvMon.Columns["TenMon"].HeaderText = "Tên Môn";
             dgvMon.Columns["DVHT"].HeaderText = "Đơn Vị Học Trình";
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            Reset();
+            btnSua.Enabled = false;
+            btnXoa.Enabled = false;
+            btnLuu.Enabled = true;
+
+            txtMaMon.Enabled = true;
+            txtMaMon.Focus();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc chắn xóa  " + txtMaMon.Text
+        + " không? Nếu có ấn nút Lưu, không thì ấn nút Hủy", "Xóa sản phẩm",
+        MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                try
+                {
+                    string sql = "delete from Mon where MaMon='" + txtMaMon.Text + "'";
+                    dt.ExcuteNonQuery(sql);
+                    string sql1 = "select * from Mon";
+                    dgvMon.DataSource = dt.TaoBang(sql1);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Xóa ko nổi.. " + ex.ToString());
+                }
+            }
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            string sql = "update Mon set TenMon='" + txtTenMon.Text
+            + "' where MaMon='" + txtMaMon.Text + "' where DVHT='" + txtDVHT.Text + "'";
+            dt.ExcuteNonQuery(sql);
+            LoadDSMon();
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            string sql = "insert into Mon values('" + txtMaMon.Text + "','"
+    + txtTenMon.Text + "','" + txtDVHT.Text + "')";
+            try
+            {
+                dt.ExcuteNonQuery(sql);
+                btnLuu.Enabled = false;
+                btnXoa.Enabled = true;
+                btnSua.Enabled = true;
+            }
+            catch
+            {
+                MessageBox.Show("Đã tồn tại dữ liệu hoặc điền thiếu/điền sai", "Úi có lỗi");
+            }
+            LoadDSMon();
+        }
+
+        private void dgvMon_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = new DataGridViewRow();
+            row = dgvMon.Rows[e.RowIndex];
+            txtMaMon.Text = Convert.ToString(row.Cells["MaMon"].Value);
+            txtTenMon.Text = Convert.ToString(row.Cells["TenMon"].Value);
+            txtDVHT.Text = Convert.ToString(row.Cells["DVHT"].Value);
         }
     }
 }
