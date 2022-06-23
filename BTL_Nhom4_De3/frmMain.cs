2da8aff105841689e59914ffcb35978f1817424e
Hải Ngọc Bùi <ngoc200h@gmail.com>
2022-06-23 23:00:42 +0700

    đổi bìa

--- BTL_Nhom4_De3/frmMain.cs
+++ BTL_Nhom4_De3/frmMain.cs
@@ -33,86 +33,99 @@ namespace BTL_Nhom4_De3
             this.pnlContent.Controls.Add(f);
             f.Show();
         }
-
-        private void thoatToolStripMenuItem_Click(object sender, FormClosingEventArgs e)
+        private void HDSDToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (MessageBox.Show("Do you want to close this application?", "Exit", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.No)
-            {
-                e.Cancel = true;
-            }
+            frmHDSD f = new frmHDSD();
+            AddForm(f);
         }
-        private void sinhVienToolStripMenuItem_Click(object sender, EventArgs e)
+
+        private void sinhVienToolStripMenuItem_Click_1(object sender, EventArgs e)
         {
             frmDSSV f = new frmDSSV();
             AddForm(f);
         }
 
-        private void khoaToolStripMenuItem1_Click_1(object sender, EventArgs e)
-        {
-            frmDSKhoa f = new frmDSKhoa();
-            AddForm(f);
-        }
-
-
-        private void diemToolStripMenuItem_Click(object sender, EventArgs e)
+        private void diemToolStripMenuItem_Click_1(object sender, EventArgs e)
         {
             frmDSDiem f = new frmDSDiem();
             AddForm(f);
         }
 
-        private void monHocToolStripMenuItem1_Click(object sender, EventArgs e)
-        {
-            frmDSMon f = new frmDSMon();
-            AddForm(f);
-        }
-
-        private void chucVuToolStripMenuItem_Click(object sender, EventArgs e)
+        private void chucVuToolStripMenuItem_Click_1(object sender, EventArgs e)
         {
             frmDSChucVu f = new frmDSChucVu();
             AddForm(f);
         }
 
-        private void queQuanToolStripMenuItem_Click(object sender, EventArgs e)
+        private void queQuanToolStripMenuItem_Click_1(object sender, EventArgs e)
         {
             frmDSQue f = new frmDSQue();
             f.StartPosition = FormStartPosition.CenterScreen;
             AddForm(f);
         }
 
-        private void danTocToolStripMenuItem_Click(object sender, EventArgs e)
+        private void danTocToolStripMenuItem_Click_1(object sender, EventArgs e)
         {
             frmDSDanToc f = new frmDSDanToc();
             AddForm(f);
         }
 
-        private void TKB_ToolStripMenuItem_Click(object sender, EventArgs e)
+        private void TKB_ToolStripMenuItem_Click_1(object sender, EventArgs e)
         {
             frmDSTKB f = new frmDSTKB();
             AddForm(f);
         }
 
-        private void phongHocToolStripMenuItem_Click(object sender, EventArgs e)
+        private void monHocToolStripMenuItem1_Click_1(object sender, EventArgs e)
+        {
+            frmDSMon f = new frmDSMon();
+            AddForm(f);
+        }
+
+        private void phongHocToolStripMenuItem_Click_1(object sender, EventArgs e)
         {
             frmDSPhongHoc f = new frmDSPhongHoc();
             AddForm(f);
         }
 
-        private void lopToolStripMenuItem_Click(object sender, EventArgs e)
+        private void lopToolStripMenuItem_Click_1(object sender, EventArgs e)
         {
             frmDSLop f = new frmDSLop();
             AddForm(f);
         }
 
-        private void chuyenNganhToolStripMenuItem_Click(object sender, EventArgs e)
+        private void khoaToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            frmDSKhoa f = new frmDSKhoa();
+            AddForm(f);
+        }
+
+        private void chuyenNganhToolStripMenuItem_Click_1(object sender, EventArgs e)
         {
             frmDSChuyenNganh f = new frmDSChuyenNganh();
             AddForm(f);
         }
 
-        private void heDTToolStripMenuItem_Click(object sender, EventArgs e)
+        private void heDTToolStripMenuItem_Click_1(object sender, EventArgs e)
         {
             frmDSHeDT f = new frmDSHeDT();
             AddForm(f);
+        }
+
+        private void thoatToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            const string message = "Ｄｏ ｙｏｕ ｗａｎｔ ｔｏ ｅｘｉｔ ?";
+            const string caption = "Thông báo nhẹ";
+            var result = MessageBox.Show(message, caption, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else if (result == DialogResult.Yes)
+            {
+                this.Close();
+
+            }
         }
     }
 }
